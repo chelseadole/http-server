d9de09af8c31c4f1d6dc27b306e10797af438117
CHELSEA DOLE <chelseadole@gmail.com>
2017-10-24 14:23:15 -0700

    fixing bugs to receive message from server

--- src/client.py
+++ src/client.py
@@ -4,21 +4,25 @@ import socket
 
 
 def client(message):
-    """Creating client socket."""
-    socket_info = socket.getaddrinfo('127.0,0.1', 5001)
+    """Client side socket."""
+    socket_info = socket.getaddrinfo('127.0.0.1', 5008)
     stream_info = [i for i in socket_info if i[1] == socket.SOCK_STREAM][0]
 
     client = socket.socket(*stream_info[:3])
     client.connect(stream_info[-1])
 
-    message = u'Un mensaje über importante con accentos éóí.'
     client.sendall(message.encode('utf8'))
 
+    reply_from_server = ''
     reply_complete = False
     while not reply_complete:
         part = client.recv(10)
-        part(part.decode('utf8'))
+        reply_from_server += part.decode('utf8')
         if len(part) < 10:
             break
+    print(reply_from_server)
 
-    # client.close()
+    client.close()
+
+if __name__ == '__main__':
+    client(u'Un mensaje über importante con accentos éóí.')
--- src/server.py
+++ src/server.py
@@ -4,9 +4,9 @@ import socket
 
 
 def server():
-    """Creating server socket."""
+    """Server side socket."""
     server = socket.socket(socket.AF_INET, socket.SOCK_STREAM, socket.IPPROTO_TCP)
-    server.bind(('127.0.0.1', 5001))
+    server.bind(('127.0.0.1', 5008))
     server.listen(1)
     conn, addr = server.accept()
 
@@ -17,23 +17,24 @@ def server():
     message_complete = False
     while not message_complete:
         part = conn.recv(buffer_length)
-        print(part.decode('utf8'))
         msg_received += part.decode('utf8')
         if len(part) < buffer_length:
             break
 
     print(msg_received)
-    response_ok()
+    conn.sendall(response_ok())
 
     conn.close()
     server.close()
 
+    if KeyboardInterrupt:
+        conn.close()
+        server.close()
+
 
 def response_ok():
-    """200 OK response for client."""
-    ok_200_msg = u'HTTP/1.1 200 OK \n Content-Type: text/plain \n <CRLF> \n Message Received.'
-    conn.sendall(ok_200_msg.encode('utf8'))
-    return ok_200_msg
+    """200 Response."""
+    return u'HTTP/1.1 200 OK \n Content-Type: text/plain \n <CRLF> \n Message Received.'.encode('utf8')
 
 
 def response_error():
@@ -41,3 +42,6 @@ def response_error():
     error_500_msg = u'HTTP/1.1 500 Internal Server Error \n Content-Type: text/plain \n <CRLF> \n Server Error.'
     conn.sendall(error_500_msg.encode('utf8'))
     return error_500_msg
+
+if __name__ == '__main__':
+    server()
