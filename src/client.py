4224d99c73bb1f8042d3f12b6d22c68836e8ee5d
CHELSEA DOLE <chelseadole@gmail.com>
2017-10-25 16:48:38 -0700

    adding shift() function, overall complete

--- src/client.py
+++ src/client.py
@@ -6,7 +6,7 @@ import socket
 
 def client(message):
     """Client side socket."""
-    socket_info = socket.getaddrinfo('127.0.0.1', 5002)
+    socket_info = socket.getaddrinfo('127.0.0.1', 5003)
     stream_info = [i for i in socket_info if i[1] == socket.SOCK_STREAM][0]
 
     client = socket.socket(*stream_info[:3])
