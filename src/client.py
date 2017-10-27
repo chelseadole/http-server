6bcfe0c8423bdc73f0cf8f313ba3e17f21917fd3
kavdihodgson <kavdyjh@gmail.com>
2017-10-27 00:30:40 -0700

    writing tests

--- src/client.py
+++ src/client.py
@@ -5,7 +5,7 @@ import socket
 
 def client(message):
     """Creating client socket."""
-    socket_info = socket.getaddrinfo('127.0.0.1', 5002)
+    socket_info = socket.getaddrinfo('127.0.0.1', 5000)
     stream_info = [i for i in socket_info if i[1] == socket.SOCK_STREAM][0]
 
     client = socket.socket(*stream_info[:3])
@@ -22,6 +22,7 @@ def client(message):
 
     print (response_msg)
     client.close()
+    return(response_msg)
 
 
 if __name__ == '__main__':
--- src/test_servers.py
+++ src/test_servers.py
@@ -1,1 +1,13 @@
 """Test client and server module."""
+
+
+import pytest
+
+
+def test_message_sent_echo_back_the_same():
+    """Test that message sent from client to server comes back."""
+    from server import server
+    from client import client
+    server()
+    x = client('hello')
+    assert x == 'hello'
