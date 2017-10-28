dc0be802ca574a1d0fe7dc46606e8cd9780db9eb
kavdihodgson <kavdyjh@gmail.com>
2017-10-27 17:13:25 -0700

    imported future

--- src/client.py
+++ src/client.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 """Client socket."""
 
+from __future__ import unicode_literals
 import socket
 
 
--- src/server.py
+++ src/server.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 """Server socket."""
 
+from __future__ import unicode_literals
 import socket
 
 
